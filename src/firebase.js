1a3a1751fdc80acb0674ef06de502c87bbba88ce
Nawin <s6602041610127@email.kmutnb.ac.th>
2025-10-07 03:33:52 +0700

    Redeploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,11 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB1ShUgyrMoLMKjBD6jrtlXeABAFVPMXy8",
   authDomain: "habit-grow-11cbf.firebaseapp.com",
-  databaseURL: "https://habit-grow-11cbf-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "habit-grow-11cbf",
   storageBucket: "habit-grow-11cbf.firebasestorage.app",
   messagingSenderId: "681468550703",
@@ -17,9 +13,8 @@ const firebaseConfig = {
   measurementId: "G-H073DDPSEP"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export const googleProvider = new GoogleAuthProvider();
