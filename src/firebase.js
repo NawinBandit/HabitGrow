35e37af13be8e99512233962119bb60bbf7f7a2e
Nawin <s6602041610127@email.kmutnb.ac.th>
2025-10-07 03:31:58 +0700

    Fix authentication with persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,15 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB1ShUgyrMoLMKjBD6jrtlXeABAFVPMXy8",
   authDomain: "habit-grow-11cbf.firebaseapp.com",
+  databaseURL: "https://habit-grow-11cbf-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "habit-grow-11cbf",
   storageBucket: "habit-grow-11cbf.firebasestorage.app",
   messagingSenderId: "681468550703",
@@ -12,8 +17,9 @@ const firebaseConfig = {
   measurementId: "G-H073DDPSEP"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
 export const googleProvider = new GoogleAuthProvider();
