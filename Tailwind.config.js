e19ff2ce091912781b8a13d1c0d5dab34d89c2e4
Nawin <s6602041610127@email.kmutnb.ac.th>
2025-10-06 13:50:45 +0700

    Fix Tailwind CSS build

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -83,6 +83,16 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms')({
       strategy: 'class',
-    }),
+    }),/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
   ],
 }
